6a714f3330e7e656f8e86a4cfe6d3a0711468742
James Faix <jamesthefaix@gmail.com>
2026-07-25 05:57:41 -0700

    Fix API serialization

--- api/api.web/Model/EventWebModel.fs
+++ api/api.web/Model/EventWebModel.fs
@@ -2,14 +2,32 @@
 
 open System
 open System.ComponentModel.DataAnnotations
+open System.Text.Json.Serialization
 open Djambi.Api.Enums
 
+[<JsonDerivedType(typeof<ChangeEffectDto<int list>>)>]
+[<JsonDerivedType(typeof<ChangeEffectDto<GameStatus>>)>]
+[<JsonDerivedType(typeof<ChangeEffectDto<PieceDto>>)>]
+[<JsonDerivedType(typeof<ChangeEffectDto<PlayerStatus>>)>]
+[<JsonDerivedType(typeof<ChangeEffectDto<TurnDto>>)>]
+[<JsonDerivedType(typeof<PieceUpdateEffectDto>)>]
+[<JsonDerivedType(typeof<NeutralPlayerAddedEffectDto>)>]
+[<JsonDerivedType(typeof<PieceEnlistedEffectDto>)>]
+[<JsonDerivedType(typeof<PieceMovedEffectDto>)>]
+[<JsonDerivedType(typeof<PieceVacatedEffectDto>)>]
+[<JsonDerivedType(typeof<PlayerAddedEffectDto>)>]
+[<JsonDerivedType(typeof<PlayerOutOfMovesEffectDto>)>]
+[<JsonDerivedType(typeof<PlayerRemovedEffectDto>)>]
+[<JsonDerivedType(typeof<PlayerStatusChangedEffectDto>)>]
+[<JsonDerivedType(typeof<TurnCyclePlayerFellFromPowerEffectDto>)>]
+[<JsonDerivedType(typeof<TurnCyclePlayerRemovedEffectDto>)>]
+[<JsonDerivedType(typeof<TurnCyclePlayerRoseToPowerEffectDto>)>]
 [<AbstractClass>]
 type EffectDto(kind : EffectKind) =
     [<Required>]
     member __.Kind = kind
 
-type ChangeEffectDto<'a>(kind : EffectKind,
+and ChangeEffectDto<'a>(kind : EffectKind,
                         oldValue : 'a,
                         newValue : 'a) =
     inherit EffectDto(kind)
@@ -18,34 +36,34 @@ type ChangeEffectDto<'a>(kind : EffectKind,
     [<Required>]
     member __.NewValue = newValue
 
-type PieceUpdateEffectDto(kind : EffectKind, oldPiece : PieceDto) =
+and PieceUpdateEffectDto(kind : EffectKind, oldPiece : PieceDto) =
     inherit EffectDto(kind)
     [<Required>]
     member __.OldPiece = oldPiece
 
-type NeutralPlayerAddedEffectDto(name : string, placeholderPlayerId : int) =
+and NeutralPlayerAddedEffectDto(name : string, placeholderPlayerId : int) =
     inherit EffectDto(EffectKind.NeutralPlayerAdded)
     [<Required>]
     member __.Name = name
     [<Required>]
     member __.PlaceholderPlayerId = placeholderPlayerId
 
-type PieceEnlistedEffectDto(oldPiece : PieceDto, newPlayerId : int) =
+and PieceEnlistedEffectDto(oldPiece : PieceDto, newPlayerId : int) =
     inherit PieceUpdateEffectDto(EffectKind.PieceEnlisted, oldPiece)
     [<Required>]
     member __.NewPlayerId = newPlayerId
 
-type PieceMovedEffectDto(oldPiece : PieceDto, newCellId : int) =
+and PieceMovedEffectDto(oldPiece : PieceDto, newCellId : int) =
     inherit PieceUpdateEffectDto(EffectKind.PieceMoved, oldPiece)
     [<Required>]
     member __.NewCellId = newCellId
 
-type PieceVacatedEffectDto(oldPiece : PieceDto, newCellId : int) =
+and PieceVacatedEffectDto(oldPiece : PieceDto, newCellId : int) =
     inherit PieceUpdateEffectDto(EffectKind.PieceVacated, oldPiece)
     [<Required>]
     member __.NewCellId = newCellId
 
-type PlayerAddedEffectDto(name : string, userId : int, playerKind : PlayerKind) =
+and PlayerAddedEffectDto(name : string, userId : int, playerKind : PlayerKind) =
     inherit EffectDto(EffectKind.PlayerAdded)
     [<Required>]
     member __.Name = name
@@ -54,32 +72,32 @@ type PlayerAddedEffectDto(name : string, userId : int, playerKind : PlayerKind) 
     [<Required>]
     member __.PlayerKind = playerKind
 
-type PlayerOutOfMovesEffectDto(playerId : int) =
+and PlayerOutOfMovesEffectDto(playerId : int) =
     inherit EffectDto(EffectKind.PlayerOutOfMoves)
     [<Required>]
     member __.PlayerId = playerId
     
-type PlayerRemovedEffectDto(oldPlayer : PlayerDto) =
+and PlayerRemovedEffectDto(oldPlayer : PlayerDto) =
     inherit EffectDto(EffectKind.PlayerRemoved)
     [<Required>]
     member __.OldPlayer = oldPlayer
 
-type PlayerStatusChangedEffectDto(oldValue : PlayerStatus, newValue : PlayerStatus, playerId : int) =
+and PlayerStatusChangedEffectDto(oldValue : PlayerStatus, newValue : PlayerStatus, playerId : int) =
     inherit ChangeEffectDto<PlayerStatus>(EffectKind.PlayerStatusChanged, oldValue, newValue)
     [<Required>]
     member __.PlayerId = playerId
 
-type TurnCyclePlayerFellFromPowerEffectDto(oldValue : List<int>, newValue : List<int>, playerId : int) =
+and TurnCyclePlayerFellFromPowerEffectDto(oldValue : List<int>, newValue : List<int>, playerId : int) =
     inherit ChangeEffectDto<List<int>>(EffectKind.TurnCyclePlayerFellFromPower, oldValue, newValue)
     [<Required>]
     member __.PlayerId = playerId
 
-type TurnCyclePlayerRemovedEffectDto(oldValue : List<int>, newValue : List<int>, playerId : int) =
+and TurnCyclePlayerRemovedEffectDto(oldValue : List<int>, newValue : List<int>, playerId : int) =
     inherit ChangeEffectDto<List<int>>(EffectKind.TurnCyclePlayerRemoved, oldValue, newValue)
     [<Required>]
     member __.PlayerId = playerId
 
-type TurnCyclePlayerRoseToPowerEffectDto(oldValue : List<int>, newValue : List<int>, playerId : int) =
+and TurnCyclePlayerRoseToPowerEffectDto(oldValue : List<int>, newValue : List<int>, playerId : int) =
     inherit ChangeEffectDto<List<int>>(EffectKind.TurnCyclePlayerRoseToPower, oldValue, newValue)
     [<Required>]
     member __.PlayerId = playerId
